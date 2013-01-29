7871656c3b619a63f9e344cae446dabfbf04041a
haks1999 <haks1999@d776d5c4-916d-c17f-a6b9-cc39338b9590>
2013-01-29 00:23:36 +0000

    repository 조회 변경

--- src/main/java/com/haks/haksvn/repository/controller/RepositoryController.java
+++ src/main/java/com/haks/haksvn/repository/controller/RepositoryController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,18 +26,20 @@ public class RepositoryController {
     private RepositoryService repositoryService;
     
     @RequestMapping(value="/list", method=RequestMethod.GET)
-    public String forwardRepositoryListPage(HttpServletRequest request, HttpServletResponse response) {
-        //List<Repository> repositoryList = repositoryService.retrieveRepositoryList();
+    public String forwardRepositoryListPage(Model model, HttpServletRequest request, HttpServletResponse response) {
+        List<Repository> repositoryList = repositoryService.retrieveRepositoryList();
+    	model.addAttribute("repositoryList", repositoryList );
         MenuUtil.setContentJspPath(request, response, "/WEB-INF/views/configuration/listRepository.jsp");
         return MenuUtil.getFowardUrl(request);
-        //return "forward:/main/configuration/repositories/list";
     }
      
+    /*
     @RequestMapping(value="/list.json", method=RequestMethod.GET, produces="application/json")
     public @ResponseBody List<Repository> retrieveRepositoryListJson() {
         List<Repository> repositoryList = repositoryService.retrieveRepositoryList();
 		return repositoryList;
     }
+    */
     
     @RequestMapping(value="/add", method=RequestMethod.GET)
     public String forwardRepositoryAddPage(HttpServletRequest request, HttpServletResponse response) {
--- src/main/webapp/WEB-INF/views/configuration/listRepository.jsp
+++ src/main/webapp/WEB-INF/views/configuration/listRepository.jsp
@@ -1,5 +1,6 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <script type="text/javascript">
+	/*
 	$(function() {
 		$.getJSON( "<c:url value="/configuration/repositories/list"/>", function(data) {
 			for( var inx = 0 ; inx < data.length ; inx++ ){
@@ -11,6 +12,7 @@
 		});
 		
    	});
+	*/
 </script>
 <div id="table" class="help">
 	<h1>Repositories</h1>
@@ -22,6 +24,13 @@
 					<th>Location</th>
 					<th>Status</th>
 				</tr>
+				<c:forEach items="${repositoryList}" var="repository">
+					<tr>
+						<td><c:out value="${repository.repositoryName}" /></td>
+						<td><c:out value="${repository.repositoryLocation}" /></td>
+						<td><c:out value="${repository.repositoryStatus}" /></td>
+					</tr>
+				</c:forEach>
 			</table>
 		</div>
 	</div>
