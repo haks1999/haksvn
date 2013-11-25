e787a5c9dd95c81f36e98f4fb7dcfb0b9fd47552
haks1999 <haks1999@d776d5c4-916d-c17f-a6b9-cc39338b9590>
2013-11-25 08:48:02 +0000

    [Request Group ID]: group-5 [Request ID]: req-6 [Request User]: administrator(admin) [Approve User]: administrator(admin) [Transfer User]: administrator(admin) [Description]: 20131125_001

--- branches/production/src/main/java/com/haks/haksvn/repository/controller/RepositoryAjaxController.java
+++ branches/production/src/main/java/com/haks/haksvn/repository/controller/RepositoryAjaxController.java
@@ -3,6 +3,9 @@ package com.haks.haksvn.repository.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.codehaus.jackson.map.DeserializationConfig;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -37,29 +40,44 @@ public class RepositoryAjaxController {
 		return message;
     }
     
-    @RequestMapping(value="/listUser/{repositorySeq}", method=RequestMethod.GET)
-    public @ResponseBody List<User> listRepositoryUser(@PathVariable int repositorySeq){
-    	List<User> userList = repositoryService.retrieveRepositoryByRepositorySeq(repositorySeq).getUserList();
+    @RequestMapping(value="/list/changeOrder", method=RequestMethod.POST)
+    public @ResponseBody ResultMessage changeOrder(@RequestParam(value = "repositoryList") String repositoryListJson) throws Exception {
+    	ResultMessage message = new ResultMessage("repository order changed success");
+    	ObjectMapper mapper = new ObjectMapper();
+		mapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+   		List<Repository> repositoryList = mapper.readValue(repositoryListJson, new TypeReference<List<Repository>>(){});
+   		repositoryService.changeRepositoryOrder(repositoryList);
+		return message;
+    }
+    
+    @RequestMapping(value="/listUser/{repositoryKey}", method=RequestMethod.GET)
+    public @ResponseBody List<User> listRepositoryUser(@PathVariable String repositoryKey){
+    	List<User> userList = repositoryService.retrieveRepositoryByRepositoryKey(repositoryKey).getUserList();
     	return userList;
     }
     
-    @RequestMapping(value="/listUser/addUser/{repositorySeq}", method=RequestMethod.POST)
-    public @ResponseBody ResultMessage addRepositoryUser(@PathVariable int repositorySeq,
+    @RequestMapping(value="/listUser/addUser/{repositoryKey}", method=RequestMethod.POST)
+    public @ResponseBody ResultMessage addRepositoryUser(@PathVariable String repositoryKey,
     												@RequestParam(value = "userId", required = true) String[] userIdList,
     												@RequestParam(value = "overwrite", required = false) boolean overwrite){
     	
     	ResultMessage message = new ResultMessage("users added");
-    	repositoryService.addRepositoryUser(Integer.valueOf(repositorySeq),Arrays.asList(userIdList),overwrite);
+    	repositoryService.addRepositoryUser(repositoryKey,Arrays.asList(userIdList),overwrite);
     	return message;
     }
     
-    @RequestMapping(value="/listUser/delUser/{repositorySeq}", method=RequestMethod.POST)
-    public @ResponseBody ResultMessage delRepositoryUser(@PathVariable int repositorySeq,
+    @RequestMapping(value="/listUser/delUser/{repositoryKey}", method=RequestMethod.POST)
+    public @ResponseBody ResultMessage delRepositoryUser(@PathVariable String repositoryKey,
     													@RequestParam(value = "userId", required = true) String[] userIdList){
     	
     	ResultMessage message = new ResultMessage("users removed");
-    	repositoryService.deleteRepositoryUser(Integer.valueOf(repositorySeq),Arrays.asList(userIdList));
+    	repositoryService.deleteRepositoryUser(repositoryKey,Arrays.asList(userIdList));
     	return message;
+    }
+    
+    @RequestMapping(value="/add/validateRepositoryKey", params ={"repositoryKey"})
+    public @ResponseBody String validateRepositoryKey(@RequestParam(value = "repositoryKey", required = true) String repositoryKey) {
+    	return String.valueOf(repositoryService.retrieveRepositoryByRepositoryKey(repositoryKey) == null);
     }
     
     @ExceptionHandler(HaksvnException.class)
