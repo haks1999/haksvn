a04973b4941fe8bf483eb0e72c8f09c86ef0a4f5
haks1999 <haks1999@d776d5c4-916d-c17f-a6b9-cc39338b9590>
2013-12-03 08:56:10 +0000

    [Tagging ID]: tagging-10 [Tagging User]: administrator(admin) [Description]: 20131203_01

---
+++ tags/20131203_01/src/main/java/com/haks/haksvn/transfer/dao/TraceSourceDao.java
@@ -0,0 +1,69 @@
+package com.haks.haksvn.transfer.dao;
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.ProjectionList;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.transform.Transformers;
+import org.hibernate.type.StandardBasicTypes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.haks.haksvn.transfer.model.TraceSourceForTransfer;
+import com.haks.haksvn.transfer.model.TransferSource;
+
+@Repository
+public class TraceSourceDao {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	// group by 되어 일단 path 만 나온다. sourceTrace autocomplete 용
+	@SuppressWarnings("unchecked")
+	public List<String> retrieveTransferSourcePathListByPath(String repositoryKey, String path, int limit){
+		Session session = sessionFactory.getCurrentSession();
+		
+		Criteria crit = session.createCriteria(TransferSource.class,"t_transferSource")
+				.createAlias("t_transferSource.transfer", "t_transfer")
+				.add(Restrictions.eq("t_transfer.repositoryKey", repositoryKey))
+				.add(Restrictions.like("t_transferSource.path", "%"+path+"%").ignoreCase())
+				.setMaxResults( limit )
+				.addOrder(Order.asc("path"));
+		ProjectionList proj = Projections.projectionList();
+		proj.add(Projections.groupProperty("path"));
+		crit.setProjection(proj);
+		return (List<String>)crit.list();
+	}
+	
+	//trace source 조회 용
+	public List<TraceSourceForTransfer> retrieveTraceSourceListByPath(String repositoryKey, String path, int limit){
+		Session session = sessionFactory.getCurrentSession();
+		
+		Query query = session.createSQLQuery("SELECT tr.transfer_seq as \"transferSeq\", tg.transfer_group_seq as \"transferGroupSeq\", ts.transfer_source_seq as \"transferSourceSeq\", tg.transfer_date as \"transferDate\", ts.revision as \"trunkRevision\", tr.revision as \"branchesRevision\" " +
+						" from transfer_group tg " + 
+						" inner join transfer tr on (tg.transfer_group_seq = tr.transfer_group_seq) " + 
+						" inner join transfer_source ts on (tr.transfer_seq = ts.transfer_seq) " + 
+						" where transfer_group_state  = 'transfergroup.state.code.transfered' " +
+						" and ts.path like '%'||:path||'%' " + 
+						" order by tg.transfer_date desc" )
+						.addScalar("transferSeq", StandardBasicTypes.INTEGER )
+						.addScalar("transferGroupSeq", StandardBasicTypes.INTEGER )
+						.addScalar("transferSourceSeq", StandardBasicTypes.INTEGER )
+						.addScalar("transferDate", StandardBasicTypes.LONG )
+						.addScalar("trunkRevision", StandardBasicTypes.LONG )
+						.addScalar("branchesRevision", StandardBasicTypes.LONG )
+						.setMaxResults(limit)
+						.setResultTransformer(Transformers.aliasToBean(TraceSourceForTransfer.class));
+		query.setString("path", path);
+		@SuppressWarnings("unchecked")
+		List<TraceSourceForTransfer> result = (List<TraceSourceForTransfer>)query.list();
+		return result;
+
+	}
+}
